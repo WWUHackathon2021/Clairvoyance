dc066c942112e3d377b985ce82b1c218b1a2ea09
David Katsandres <dkatsandres@hotmail.com>
2021-04-10 15:55:34 -0700

    Remove MainView, add StyleSheet for CameraView.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
-import MainView from './views/MainView';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from './views/MapView';
+import CameraView from './views/CameraView';
 
 const App = () => {
-  return MainView();
+  const [currentCamera, setCurrentCamera] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <MapView setCameraFunction = { setCurrentCamera } />
+      <CameraView cameraUrl = { currentCamera } />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
 
 export default App;
--- views/CameraView.tsx
+++ views/CameraView.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Image, View } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 type CameraProps = {
   cameraUrl: string
@@ -8,14 +8,27 @@ type CameraProps = {
 const CameraView: FunctionComponent<CameraProps> = ({ cameraUrl }: CameraProps) => {
   return (
     <>
-      <View style={{ flex: 1, backgroundColor: "green" }}>
-      <Image
-        source={{uri: cameraUrl}}
-        style={{width: 400, height: 400}}
-      />
+      <View style={styles.view}>
+        <Image
+          source={{uri: cameraUrl}}
+          style={styles.image}
+        />
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "green"
+  },
+  image: {
+    width: 400,
+    height: 400
+  }
+});
 
 export default CameraView;
